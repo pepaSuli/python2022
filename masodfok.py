d71d4d444c2d40f78c8c7239c9565da8ca9c376e
pepaSuli <papp.peter@ady-nagyatad.hu>
2022-11-17 14:38:16 +0100

    valami

--- masodfok.py
+++ masodfok.py
@@ -1,5 +1,24 @@
 import math
 
+def egyenlet(a,b,c):
+    szoveg="0 = "
+    if a!=0:
+        szoveg+=str(a)+"x²"
+
+    if b>0:
+        szoveg+=" + "+str(b)+"x"
+    elif b<0:
+        szoveg+=" "+str(b)+"x"
+        
+    if c>0:
+        szoveg+=" + "+str(c)
+    elif c<0:
+        szoveg+=" "+str(c)
+        
+    return szoveg
+    
+
+
 #a*X2+b*x+c
 
 a = int(input("a="))
@@ -9,11 +28,17 @@ c = int(input("c="))
 #(-b+gyök(b2-4ac)) / 2a
 
 #print(math.sqrt(3))
+
+x1=""
+x2=""
+
 diszkriminans=b*b-4*a*c
 if diszkriminans<0:
     print("nincs megoldás")
 elif diszkriminans==0:
     megoldas=-b /(2*a)
+    x1=megoldas
+    x2=megoldas
     print("1 megoldás: {}".format(megoldas))
 else:
     x1=(-b+math.sqrt(diszkriminans)) / (2*a)
@@ -22,4 +47,17 @@ else:
 
 #gyok=math.sqrt(1)
 #print(gyok)
+print(egyenlet(a,b,c))
+
+#a*(x-x1)*(x-x2)=0
+print(a)
+print(x1)
+print(x2)
+
+
+
+
+
+
+
 
